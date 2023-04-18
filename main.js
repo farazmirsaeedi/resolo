c2059e6231cc5bbc51c5fcdde014968dc2e2826a
farazmirsaeedi <farazmirsaeedi@gmail.com>
2023-04-19 00:15:47 +0430

    updated

--- main.js
+++ main.js
@@ -3,13 +3,26 @@ var hait = document.getElementsByClassName('hait')[0];
 var total = document.getElementsByClassName('total')[0];
 var btnref = document.getElementsByClassName('btnref')[0];
 
+var wid2 = document.getElementsByClassName('wid2')[0];
+var hait2 = document.getElementsByClassName('hait2')[0];
+var total2 = document.getElementsByClassName('total2')[0];
 
+var screenw = window.screen.width;
+var screenh = window.screen.height;
 
 wid.innerHTML += innerWidth;
 hait.innerHTML += innerHeight;
 total.innerHTML = innerWidth + "x" + innerHeight;
 
+// if(wid2 != null)
+
+wid2.innerHTML += screenw
+hait2.innerHTML += screenh;
+total2.innerHTML = screenw + "x" + screenh;
+
+
+
 // btnref.addEventListener("onclick",refresh)
-function refresh(){
-window.location.reload();
+function refresh() {
+    window.location.reload();
 }
